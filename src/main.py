f88812123b5f4eefb9e3485e902f270f7967a0e9
Steffen Parth <steffen.parth@icloud.com>
2025-08-23 14:35:41 +0200

    feat(main.py notifications post method implemented with basic logging and basic response. Different response if its a warning)

--- src/main.py
+++ src/main.py
@@ -1,7 +1,24 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+
+saved_notifications = []
+
+class DefaultNotification(BaseModel):
+    Type: str
+    Name: str
+    Description: str
+
+
+@app.post("/notifications")
+def read_root(notification: DefaultNotification):
+
+    saved_notifications.append(notification)
+
+    if notification.Type == "Warning":
+        print("Warning received")
+        return {"message": "Warning received"}
+    print("message received")
+    return {"message": "message received"}
